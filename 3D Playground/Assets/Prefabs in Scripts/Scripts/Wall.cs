c3dd5fb3de05237bcf403079611ac5ebeda4c414
Ethan Evans <42775811+EthanEvans2022@users.noreply.github.com>
2021-03-23 20:28:08 -0500

    Worked through half of "Instantiating Prefabs at run time" part of manual

---
+++ 3D Playground/Assets/Prefabs in Scripts/Scripts/Wall.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+public class Wall : MonoBehaviour
+{
+    public GameObject block;
+    public int width = 10;
+    public int height = 4;
+
+    void Start()
+    {
+        for (int y = 0; y < height; ++y)
+        {
+            for (int x = 0; x < width; ++x)
+            {
+                Instantiate(block, new Vector3(x, y, 5), Quaternion.identity);
+            }
+        }
+    }
+}
